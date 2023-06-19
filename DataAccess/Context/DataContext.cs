be65c31fa11c5903ae87fc590181712116d74bc5
Breno Albuquerque <brenoralbu@hotmail.com>
2023-06-18 22:56:15 -0300

    [CHORE] - Estruturando solução

---
+++ DataAccess/Context/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SqlDataAccess.Context
+{
+    static internal class DataContext
+    {
+        public const string ConnectionString = "Server=localhost,1433;Database=SQL_RESERVATION_SYSTEM;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+
+        static public SqlConnection GetConnection() => new SqlConnection(ConnectionString);
+    }
+}
