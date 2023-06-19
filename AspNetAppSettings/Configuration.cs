f1bef028559d4c5f3d859459647ef3be98f3a960
Breno Albuquerque <brenoralbu@hotmail.com>
2023-06-18 22:51:56 -0300

    [FEAT] - AspNetAppSettings

---
+++ AspNetAppSettings/Configuration.cs
@@ -0,0 +1,18 @@
+﻿//  Criando classe Configuration como estática
+public static class Configuration
+{
+    public static string ApiKeyName { get; set; }
+
+    public static string JwtKey { get; set; }
+
+    public static bool IsEnabled { get; set; }
+
+    public static SmtpConfiguration SmtpConfiguration { get; set; }
+}
+
+public class SmtpConfiguration
+{
+    public string Host { get; set; }
+
+    public int Port { get; set; }
+}
