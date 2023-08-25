58d03a66ef244bf0b52a13f04e8b91799e943d27
Breno Albuquerque <brenoralbu@hotmail.com>
2023-08-25 08:55:42 -0300

    [FEAT] - FluentBuilder

---
+++ FluentBuilder/Program.cs
@@ -0,0 +1,15 @@
+﻿using BasicBuilder;
+
+// With builder
+var builder = new HtmlBuilder("ul")
+    .AddChild("li", "c#")
+    .AddChild("li", "Java")
+    .Build(); // Returns HtmlElement
+
+// Without builder
+var element = new HtmlElement("ul");
+element.Elements.Add(new HtmlElement("li", "Go"));
+element.Elements.Add(new HtmlElement("li", "Flutter"));
+
+Console.WriteLine(builder);
+Console.WriteLine(element);
