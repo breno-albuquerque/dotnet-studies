b643efee7478ad35c3011e28c8adba430cf96c4e
Breno Albuquerque <brenoralbu@hotmail.com>
2023-09-02 18:33:40 -0300

    [FEAT] - PerThreadSingleton

---
+++ DesignPatterns/Singleton/PerThreadSingleton/Program.cs
@@ -0,0 +1,28 @@
+﻿// Demo
+var task1 = Task.Factory.StartNew(() =>
+{
+    Console.WriteLine("Task 1: " + SingletonFoo.Instance.Id);
+    Console.WriteLine("Task 1: " + SingletonFoo.Instance.Id);
+});
+
+var task2 = Task.Factory.StartNew(() =>
+{
+    Console.WriteLine("Task 2: " + SingletonFoo.Instance.Id);
+    Console.WriteLine("Task 2: " + SingletonFoo.Instance.Id);
+});
+
+Task.WaitAll(task1, task2);
+
+public class SingletonFoo
+{
+    private static readonly ThreadLocal<SingletonFoo> ThreadInstance = new(() => new SingletonFoo());
+
+    public static SingletonFoo Instance => ThreadInstance.Value!;
+
+    public int Id { get; }
+    
+    private SingletonFoo()
+    {
+        Id = Thread.CurrentThread.ManagedThreadId;
+    }
+}
