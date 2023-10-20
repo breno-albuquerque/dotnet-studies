f1dde01dc5b5c7a5e6687bd8af753586c714eb0e
Breno Albuquerque <brenoralbu@hotmail.com>
2023-10-19 21:42:29 -0300

    [FEAT] - Flyweight

---
+++ DesignPatterns/Flyweight/FlyweightImplementation/Program.cs
@@ -0,0 +1,73 @@
+﻿var carType1 = CarTypeFactory.GetCarType("Peugeot", "208");
+var carType2 = CarTypeFactory.GetCarType("Lamborghini", "Urus");
+var carType3 = CarTypeFactory.GetCarType("Lamborghini", "Urus");
+
+var car1 = new Car(150, "Belo Horizonte", "New York", carType1);
+var car2 = new Car(200, "Belo Horizonte", "New York", carType2);
+var car3 = new Car(200, "Belo Horizonte", "New York", carType3);
+
+Console.WriteLine("Reference Equals CarType1 & CarType2: " + ReferenceEquals(car1.CarType, car2.CarType));
+Console.WriteLine("Reference Equals CarType2 & CarType3: " + ReferenceEquals(car2.CarType, car3.CarType));
+
+Console.WriteLine(car1.Move());
+Console.WriteLine(car2.Move());
+Console.WriteLine(car3.Move());
+
+// Objeto Flyweight (Possui o estado intrínseco)
+public class CarType
+{
+    public string Brand { get; }
+
+    public string Model { get; }
+    
+
+    public CarType(string brand, string model)
+    {
+        Brand = brand;
+        Model = model;
+    }
+
+    // Recebe o estado extrínseco via parâmetros
+    public string Move(float speed, string from, string to)
+        => $"Car {Brand} {Model} moving at {speed} kilometers per hour from {from} to {to}";
+}
+
+public class Car
+{
+    public float Speed { get; }
+
+    public string Location { get; }
+
+    public string Destination { get; }
+
+    public CarType CarType { get; }
+
+    public Car(float speed, string location, string destination, CarType carType)
+    {
+        Speed = speed;
+        Location = location;
+        Destination = destination;
+        CarType = carType;
+    }
+
+    public string Move() => CarType.Move(Speed, Location, Destination);
+}
+
+// Factory para decidir se reutilizará o CarType ou criará um novo
+public static class CarTypeFactory
+{  
+    private static readonly List<CarType> _carTypes = new();  
+	  
+    public static CarType GetCarType(string brand, string model)
+    {
+        var carType = _carTypes.Find((carType) => carType.Brand == brand && carType.Model == model);
+
+        if (carType is null)
+        {
+            carType = new CarType(brand, model);
+            _carTypes.Add(carType);
+        }
+        
+        return carType;
+    }  
+}
---
+++ DesignPatterns/Flyweight/FlyweightWithStringBehavior/Program.cs
@@ -0,0 +1,60 @@
+﻿var names = new List<string>() { "John", "Titor" };
+// ---- TRACK REFERENCES: "John" (1); "Titor" (2)
+
+// Demo user
+var user1 = new User($"{names[0]} {names[1]}");
+// ---- TRACK REFERENCES: "John Titor" (3)
+
+var user2 = new User($"{names[0]} {names[1]}");
+// ---- TRACK REFERENCES: "Titor John" (4)
+
+// // Demo betterUser
+var betterUser1 = new BetterUser($"{names[0]} {names[1]}");
+// ---- TRACK REFERENCES: mantém 2
+
+var betterUser2 = new BetterUser($"{names[0]} {names[1]}");
+// ---- TRACK REFERENCES: mantém 2
+
+Console.WriteLine("User 1: " + user1.FullName);
+Console.WriteLine("User 2: " + user2.FullName);
+Console.WriteLine("BetterUser 1: " + betterUser1.FullName);
+Console.WriteLine("BetterUser 2: " + betterUser2.FullName);
+
+public class User
+{
+    public string FullName { get; set; }
+    
+    public User(string fullName)
+    {
+        FullName = fullName;
+    }
+}
+
+public class BetterUser
+{
+    private static List<string> Strings { get; } = new();
+    
+    public string FullName => string.Join(" ", _names.Select(i => Strings[i]));
+
+    private readonly int[] _names;
+
+    public BetterUser(string fullName)
+    {
+        int GetOrAdd(string s)
+        {
+            int idx = Strings.IndexOf(s);
+
+            if (idx != -1) return idx;
+            
+            Strings.Add(s);
+            return Strings.Count - 1;
+        }
+
+        _names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+    }
+}
+
+
+
+
+
