548801328f3774c5c37cb192183025f2f2adaea7
Breno Albuquerque <brenoralbu@hotmail.com>
2023-12-10 21:28:38 -0300

    [FEAT] - StateWithSwitchExpressions

---
+++ DesignPatterns/State/StateWithSwitchExpressions/Program.cs
@@ -0,0 +1,37 @@
+﻿// Demo
+var vault = Vault.Locked;
+
+Console.WriteLine(vault);
+
+Console.WriteLine(vault = VaultHandler.Handle(vault, Action.TypePassword, false));
+Console.WriteLine(vault = VaultHandler.Handle(vault, Action.TypePassword, true));
+
+public static class VaultHandler
+{
+    public static Vault Handle(Vault vault, Action action, bool passwordMatches) =>
+        (vault, action, passwordMatches) switch
+        {
+            (Vault.Opened, Action.Close, _) => Vault.Opened,
+            (Vault.Closed, Action.Open, _) => Vault.Opened,
+            (Vault.Locked, Action.TypePassword, true) => Vault.Closed,
+            (Vault.Locked, Action.TypePassword, false) => Vault.PermanentlyLocked,
+            _ => vault
+        };
+}
+
+
+public enum Vault
+{
+    Opened,
+    Closed,
+    Locked,
+    PermanentlyLocked
+}
+
+public enum Action
+{
+    Open,
+    Close,
+    TypePassword,
+}
+
