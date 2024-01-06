54ad31adec02090e4439eedefc2b6860ba131ee3
Breno Albuquerque <brenoralbu@hotmail.com>
2024-01-06 13:06:23 -0300

    [FEAT] - VisitorAndDoubleDispatch

---
+++ DesignPatterns/Visitor/VisitorAndDoubleDispatch/Program.cs
@@ -0,0 +1,82 @@
+﻿// Demo
+var visitor1 = new ConcreteVisitor1();
+var visitor2 = new ConcreteVisitor2();
+
+var componentA = new ConcreteComponentA();
+var componentB = new ConcreteComponentB();
+
+componentA.Accept(visitor1);
+componentA.Accept(visitor2);
+componentB.Accept(visitor1);
+componentB.Accept(visitor2);
+
+public interface IComponent
+{
+    void Accept(IVisitor visitor);
+}
+
+public class BaseComponent : IComponent
+{
+    public virtual void Accept(IVisitor visitor)
+    {
+        
+    }
+}
+
+public class ConcreteComponentA : BaseComponent
+{
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this); // double dipatch
+    }
+    public string ExclusiveMethodOfConcreteComponentA()
+    {
+        return "A";
+    }
+}
+
+public class ConcreteComponentB : BaseComponent
+{
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this); // double dipatch
+    }
+
+    public string SpecialMethodOfConcreteComponentB()
+    {
+        return "B";
+    }
+}
+
+public interface IVisitor
+{
+    void Visit(ConcreteComponentA element);
+
+    void Visit(ConcreteComponentB element);
+}
+
+class ConcreteVisitor1 : IVisitor
+{
+    public void Visit(ConcreteComponentA element)
+    {
+        Console.WriteLine(element.ExclusiveMethodOfConcreteComponentA() + " + ConcreteVisitor1");
+    }
+
+    public void Visit(ConcreteComponentB element)
+    {
+        Console.WriteLine(element.SpecialMethodOfConcreteComponentB() + " + ConcreteVisitor1");
+    }
+}
+
+class ConcreteVisitor2 : IVisitor
+{
+    public void Visit(ConcreteComponentA element)
+    {
+        Console.WriteLine(element.ExclusiveMethodOfConcreteComponentA() + " + ConcreteVisitor2");
+    }
+
+    public void Visit(ConcreteComponentB element)
+    {
+        Console.WriteLine(element.SpecialMethodOfConcreteComponentB() + " + ConcreteVisitor2");
+    }
+}
