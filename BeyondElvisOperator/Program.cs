ee6aa62472f30eac864e970e57506c5fc90d0016
Breno Albuquerque <brenoralbu@hotmail.com>
2024-02-05 13:27:56 -0300

    [FEAT] - BeyondElvisOperator

---
+++ BeyondElvisOperator/Program.cs
@@ -0,0 +1,77 @@
+﻿// Demo
+var person = new Person();
+
+var postcode = person.With(x => x.Address).With(x => x.PostCode);
+
+var postcode2 = person
+    .If(HasMedicalRecord)
+    .With(x => x.Address)
+    .Do(CheckAddress)
+    .Return(x => x.PostCode, "UNKNOWN");
+
+Console.WriteLine(postcode, postcode2);
+
+// Forma de fazer verificações de nulo indo além de simplesmente utilizar o elvis operator
+void CheckAddress(Address pAddress)
+{
+    throw new NotImplementedException();
+}
+
+bool HasMedicalRecord(Person person)
+{
+    throw new NotImplementedException();
+}
+
+public static class Maybe
+{
+    public static TResult? With<TInput, TResult>(this TInput? o, Func<TInput, TResult> evaluator)
+        where TResult : class
+        where TInput : class
+    {
+        if (o is null) return null;
+        
+        return evaluator(o);
+    }
+
+    public static TInput? If<TInput>(this TInput? o, Func<TInput, bool> evaluator)
+        where TInput : class
+    {
+        if (o is null) return null;
+        
+        return evaluator(o) ? o : null;
+    }
+
+    public static TInput? Do<TInput>(this TInput? o, Action<TInput> action)
+        where TInput : class
+    {
+        if (o is null) return null;
+        
+        action(o);
+        
+        return o;
+    }
+
+    public static TResult Return<TInput, TResult>(this TInput? o, Func<TInput, TResult> evaluator, TResult failureValue)
+        where TInput : class
+    {
+        if (o is null) return failureValue;
+        
+        return evaluator(o);
+    }
+
+    public static TResult WithValue<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator)
+        where TInput : struct
+    {
+        return evaluator(o);
+    }
+}
+
+public class Person
+{
+    public Address Address { get; set; }
+}
+
+public class Address
+{
+    public string PostCode { get; set; }
+}
