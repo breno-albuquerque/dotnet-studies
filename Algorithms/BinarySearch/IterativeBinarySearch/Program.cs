30edfbee8958fb17b14c6305c138eb70b245ff25
Breno Albuquerque <brenoralbu@hotmail.com>
2024-03-17 12:50:33 -0300

    [FEAT] - IterativeBinarySearch

---
+++ Algorithms/BinarySearch/IterativeBinarySearch/Program.cs
@@ -0,0 +1,31 @@
+﻿// Binary Search
+// Big-O notation: O(log n)
+
+// Array needs to be in sorted order
+var arr = new int[] { 15, 21, 47, 96, 108, 297 };
+
+Console.WriteLine(IterativeBinarySearch(arr, arr.Length, 21));
+Console.WriteLine(IterativeBinarySearch(arr, arr.Length, 108));
+Console.WriteLine(IterativeBinarySearch(arr, arr.Length, 297));
+
+
+int IterativeBinarySearch(int[] arr, int length, int element)
+{
+    int start = 0;
+    int end = length - 1;
+    
+    while (start <= end)
+    {
+        int mid = (end + start) / 2;
+
+        if (element == arr[mid])
+            return mid;
+
+        if (element > arr[mid])
+            start = mid + 1;
+        else if (element < arr[mid])
+            end = mid - 1;
+    }
+
+    return -1;
+}
