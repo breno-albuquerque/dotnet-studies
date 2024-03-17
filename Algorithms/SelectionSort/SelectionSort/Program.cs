f18d313d9a5f737603f3b144215ac3204fdf915c
Breno Albuquerque <brenoralbu@hotmail.com>
2024-03-17 16:26:28 -0300

    [FEAT] - SelectionSort

---
+++ Algorithms/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,25 @@
+﻿// Selection Sort
+// Unstable Algorithm
+// Big-O notation: O(n^2)
+
+// Demo
+var arr = new int[] { 84, 21, 47, 96, 15 };
+
+SelectionSort(arr, arr.Length);
+
+foreach (var number in arr)
+    Console.WriteLine(number);
+
+void SelectionSort(int[] arr, int length)
+{
+    for (int i = 0; i < length - 1; i++)
+    {
+        int pos = i;
+
+        for (int j = i + 1; j < length; j++)
+            if (arr[j] < arr[pos])
+                pos = j;
+        
+        (arr[i], arr[pos]) = (arr[pos], arr[i]);
+    }
+}
