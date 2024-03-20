4d35f263c521b8f389e072cb98bd02b5878ba3c2
Breno Albuquerque <brenoralbu@hotmail.com>
2024-03-20 07:02:37 -0300

    [FEAT] - InsertionSort

---
+++ Algorithms/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿// Insertion Sort
+// Stable Algorithm
+// Big-O notation: O(n^2)
+
+// Demo
+var arr = new int[] { 84, 21, 47, 96, 15 };
+
+InsertionSort(arr, arr.Length);
+
+foreach (var number in arr)
+    Console.WriteLine(number);
+
+void InsertionSort(int[] arr, int length)
+{
+    for (int i = 1; i < length; i++)
+    {
+        int temp = arr[i];
+        int pos = i;
+
+        while (pos > 0 && arr[pos - 1] > temp)
+        {
+            arr[pos] = arr[pos - 1];
+            pos--;
+        }
+        
+        arr[pos] = temp;
+    }
+}
